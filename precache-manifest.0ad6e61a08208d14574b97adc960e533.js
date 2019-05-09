e69e0198cd52eba5b8e21336f67c3e298e097bfe
pkozhushko <petro.kozhushko17@gmail.com>
2019-05-09 07:45:23 +0300

    Updates

---
+++ precache-manifest.0ad6e61a08208d14574b97adc960e533.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f88ef02e73fb8039b39801893200733",
+    "url": "/todoList/static/media/male.4f88ef02.png"
+  },
+  {
+    "revision": "64954d103018787e159f4d9879c3b092",
+    "url": "/todoList/static/media/main.64954d10.png"
+  },
+  {
+    "revision": "1271b75158f193c1cb90cad38dd1391d",
+    "url": "/todoList/static/media/female.1271b751.png"
+  },
+  {
+    "revision": "5fd5f1b880b0caddb620",
+    "url": "/todoList/static/js/runtime~main.6fc37664.js"
+  },
+  {
+    "revision": "b824704a3229577554c8",
+    "url": "/todoList/static/js/main.edd49c03.chunk.js"
+  },
+  {
+    "revision": "49087ad10cd3232c1302",
+    "url": "/todoList/static/js/2.019590f5.chunk.js"
+  },
+  {
+    "revision": "b824704a3229577554c8",
+    "url": "/todoList/static/css/main.dae1e13a.chunk.css"
+  },
+  {
+    "revision": "9b2dd2d561f6a92194942e47f6b3ef40",
+    "url": "/todoList/index.html"
+  }
+];
